6a78ec528ec06cff49bcec3e440877b4ed7871d7
Mickael Gaspar <migatsar@gmail.com>
2021-09-24 11:48:28 +0200

    Added view functions for posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from blog.models import Comment, Post
+
 
 # Create your views here.
 def blog(request):
     return render(request, 'blog.html', {})
+
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created-on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contain=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments
+    }
+    return render(request, "blog_detail.html", context)
